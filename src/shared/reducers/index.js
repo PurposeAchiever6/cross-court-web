4e605dc6f7de4d5537dbdbe07475e5cb8a37d63c
Carlos Puga <carlos.puga@rootstrap.com>
2019-11-14 12:16:56 -0300

    chore(*): Adds immer todo

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
+// Implement immer.js
+
 export default history =>
   combineReducers({
     router: connectRouter(history),
