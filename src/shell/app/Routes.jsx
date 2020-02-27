ead31ed0efd44e59b783b1b9c0ee9eb7c9c0eb7c
Carlos Puga <carlos.puga@rootstrap.com>
2020-02-27 08:43:10 -0500

    feat(*): Adds lazy loading for routes

--- src/shell/app/Routes.jsx
+++ src/shell/app/Routes.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+/* eslint-disable import/first */
+
+import React, { useEffect, Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import styled from 'styled-components';
@@ -6,45 +8,46 @@ import { ToastContainer, Zoom } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import ReactGA from 'react-ga';
 
-import { getIsAuthenticated } from 'screens/auth/reducer';
-import { history } from 'shared/history';
-import colors from 'shared/styles/constants';
+import { initialAppLoad } from 'shared/actions/actionCreators';
 import ROUTES from 'shared/constants/routes';
-import { getLegalDocs } from 'screens/legal-docs/actionCreators';
-import Home from 'screens/homepage/HomePage';
-import Login from 'screens/auth/pages/LoginPage';
-import Signup from 'screens/auth/pages/SignupPage';
-import SignupSuccess from 'screens/auth/pages/SignupSuccess';
-import SignupConfirmation from 'screens/auth/pages/SignupConfirmation';
-import ForgotPass from 'screens/auth/pages/ForgotPassPage';
-import ForgotPassSuccess from 'screens/auth/pages/ForgotPassSuccess';
-import PassReset from 'screens/auth/pages/PassResetPage';
-import PassResetSuccess from 'screens/auth/pages/PassResetSuccess';
-import SemSession from 'screens/sem-session/semSession';
-import SessionState from 'screens/sem-session/pages/sessionState';
-import HowItWorks from 'screens/how-it-works/HowItWorksPage';
-import SemHomePage from 'screens/sem/semHomePage';
-import Locations from 'screens/locations/LocationsPage';
-import Sessions from 'screens/sessions/SessionsPage';
-import SessionConfirmed from 'screens/sessions/pages/SessionConfirmed';
-import SessionReserved from 'screens/sessions/pages/SessionReserved';
-import Checkout from 'screens/checkout/CheckoutPage';
-import SeriesPage from 'screens/series/SeriesPage';
-import MyAccount from 'screens/my-account/MyAccountPage';
-import PurchaseHistory from 'screens/purchase-history/PurchaseHistoryPage';
-import CheckoutConfirm from 'screens/checkout/pages/CheckoutConfirm';
-import Payments from 'screens/payments/PaymentsPage';
-import PaymentsAddCard from 'screens/payments/pages/AddCard';
-import SemHandbook from 'shared/pages/SemHandbook';
-import FAQ from 'shared/pages/Faq';
-import CancelationPolicy from 'screens/legal-docs/pages/CancelationPolicy';
-import TermsAndConditions from 'screens/legal-docs/pages/TermsAndConditions';
 import Header from 'shared/components/Header';
 import Footer from 'shared/components/Footer';
-import { initialAppLoad } from 'shared/actions/actionCreators';
+import Loading from 'shared/components/Loading';
 import ScrollToPosition from 'shared/components/ScrollToPosition';
-
+import { history } from 'shared/history';
+import colors from 'shared/styles/constants';
+import { getIsAuthenticated } from 'screens/auth/reducer';
+import { getLegalDocs } from 'screens/legal-docs/actionCreators';
 import PrivateRoute from './PrivateRoute';
+
+const Home = lazy(() => import('screens/homepage/HomePage'));
+const Login = lazy(() => import('screens/auth/pages/LoginPage'));
+const Signup = lazy(() => import('screens/auth/pages/SignupPage'));
+const SignupSuccess = lazy(() => import('screens/auth/pages/SignupSuccess'));
+const SignupConfirmation = lazy(() => import('screens/auth/pages/SignupConfirmation'));
+const ForgotPass = lazy(() => import('screens/auth/pages/ForgotPassPage'));
+const ForgotPassSuccess = lazy(() => import('screens/auth/pages/ForgotPassSuccess'));
+const PassReset = lazy(() => import('screens/auth/pages/PassResetPage'));
+const PassResetSuccess = lazy(() => import('screens/auth/pages/PassResetSuccess'));
+const SemSession = lazy(() => import('screens/sem-session/semSession'));
+const SessionState = lazy(() => import('screens/sem-session/pages/sessionState'));
+const HowItWorks = lazy(() => import('screens/how-it-works/HowItWorksPage'));
+const SemHomePage = lazy(() => import('screens/sem/semHomePage'));
+const Locations = lazy(() => import('screens/locations/LocationsPage'));
+const Sessions = lazy(() => import('screens/sessions/SessionsPage'));
+const SessionConfirmed = lazy(() => import('screens/sessions/pages/SessionConfirmed'));
+const SessionReserved = lazy(() => import('screens/sessions/pages/SessionReserved'));
+const Checkout = lazy(() => import('screens/checkout/CheckoutPage'));
+const SeriesPage = lazy(() => import('screens/series/SeriesPage'));
+const MyAccount = lazy(() => import('screens/my-account/MyAccountPage'));
+const PurchaseHistory = lazy(() => import('screens/purchase-history/PurchaseHistoryPage'));
+const CheckoutConfirm = lazy(() => import('screens/checkout/pages/CheckoutConfirm'));
+const Payments = lazy(() => import('screens/payments/PaymentsPage'));
+const PaymentsAddCard = lazy(() => import('screens/payments/pages/AddCard'));
+const SemHandbook = lazy(() => import('shared/pages/SemHandbook'));
+const FAQ = lazy(() => import('shared/pages/Faq'));
+const CancelationPolicy = lazy(() => import('screens/legal-docs/pages/CancelationPolicy'));
+const TermsAndConditions = lazy(() => import('screens/legal-docs/pages/TermsAndConditions'));
 
 const AppWrapper = styled.div`
   font-family: 'Untitled Sans';
@@ -99,99 +102,101 @@ const Routes = () => {
         closeButton={false}
         bodyClassName="toaster-container"
       />
-      <ConnectedRouter history={history}>
-        <Header />
-        <ScrollToPosition />
-        <main>
-          <Switch>
-            <Route path={ROUTES.LOGIN}>
-              <Login />
-            </Route>
-            <Route path={ROUTES.SIGNUP} exact>
-              <Signup />
-            </Route>
-            <Route path={ROUTES.SIGNUPSUCCESS}>
-              <SignupSuccess />
-            </Route>
-            <Route path={ROUTES.SIGNUPCONFIRMATION}>
-              <SignupConfirmation />
-            </Route>
-            <Route path={ROUTES.FORGOTPASSWORD} exact>
-              <ForgotPass />
-            </Route>
-            <Route path={ROUTES.FORGOTPASSWORDSUCCESS}>
-              <ForgotPassSuccess />
-            </Route>
-            <Route path={ROUTES.RESETPASSWORD} exact>
-              <PassReset />
-            </Route>
-            <Route path={ROUTES.RESETPASSWORDSUCCESS}>
-              <PassResetSuccess />
-            </Route>
-            <Route path={ROUTES.LOCATIONS}>
-              <Locations />
-            </Route>
-            <Route path={ROUTES.SESSION} exact>
-              <Sessions />
-            </Route>
-            <Route path={ROUTES.SESSIONRESERVED}>
-              <SessionReserved />
-            </Route>
-            <Route path={ROUTES.SESSIONCONFIRMED}>
-              <SessionConfirmed />
-            </Route>
-            <Route path={ROUTES.HOWITWORKS}>
-              <HowItWorks />
-            </Route>
-            <Route path={ROUTES.SEM} exact>
-              <SemHomePage />
-            </Route>
-            <Route path={ROUTES.SERIES}>
-              <SeriesPage />
-            </Route>
-            <Route path={ROUTES.TERMS}>
-              <TermsAndConditions />
-            </Route>
-            <Route path={ROUTES.CANCELATIONPOLICY}>
-              <CancelationPolicy />
-            </Route>
-            <PrivateRoute path={ROUTES.PURCHASEHISTORY}>
-              <PurchaseHistory />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.CHECKOUT} exact>
-              <Checkout />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.CHECKOUTCONFIRMED}>
-              <CheckoutConfirm />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.PAYMENTS} exact>
-              <Payments />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.PAYMENTSADDCARD}>
-              <PaymentsAddCard />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.MYACCOUNT}>
-              <MyAccount />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.SEMSESSION} exact>
-              <SemSession />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.SESSIONSTATE} exact>
-              <SessionState />
-            </PrivateRoute>
-            <PrivateRoute path={ROUTES.SEMHANDBOOK}>
-              <SemHandbook />
-            </PrivateRoute>
-            <Route path={ROUTES.FAQ}>
-              <FAQ />
-            </Route>
-            <Route path={ROUTES.HOME} exact>
-              <Home />
-            </Route>
-          </Switch>
-        </main>
-        <Footer />
-      </ConnectedRouter>
+      <Suspense fallback={Loading}>
+        <ConnectedRouter history={history}>
+          <Header />
+          <ScrollToPosition />
+          <main>
+            <Switch>
+              <Route path={ROUTES.LOGIN}>
+                <Login />
+              </Route>
+              <Route path={ROUTES.SIGNUP} exact>
+                <Signup />
+              </Route>
+              <Route path={ROUTES.SIGNUPSUCCESS}>
+                <SignupSuccess />
+              </Route>
+              <Route path={ROUTES.SIGNUPCONFIRMATION}>
+                <SignupConfirmation />
+              </Route>
+              <Route path={ROUTES.FORGOTPASSWORD} exact>
+                <ForgotPass />
+              </Route>
+              <Route path={ROUTES.FORGOTPASSWORDSUCCESS}>
+                <ForgotPassSuccess />
+              </Route>
+              <Route path={ROUTES.RESETPASSWORD} exact>
+                <PassReset />
+              </Route>
+              <Route path={ROUTES.RESETPASSWORDSUCCESS}>
+                <PassResetSuccess />
+              </Route>
+              <Route path={ROUTES.LOCATIONS}>
+                <Locations />
+              </Route>
+              <Route path={ROUTES.SESSION} exact>
+                <Sessions />
+              </Route>
+              <Route path={ROUTES.SESSIONRESERVED}>
+                <SessionReserved />
+              </Route>
+              <Route path={ROUTES.SESSIONCONFIRMED}>
+                <SessionConfirmed />
+              </Route>
+              <Route path={ROUTES.HOWITWORKS}>
+                <HowItWorks />
+              </Route>
+              <Route path={ROUTES.SEM} exact>
+                <SemHomePage />
+              </Route>
+              <Route path={ROUTES.SERIES}>
+                <SeriesPage />
+              </Route>
+              <Route path={ROUTES.TERMS}>
+                <TermsAndConditions />
+              </Route>
+              <Route path={ROUTES.CANCELATIONPOLICY}>
+                <CancelationPolicy />
+              </Route>
+              <PrivateRoute path={ROUTES.PURCHASEHISTORY}>
+                <PurchaseHistory />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.CHECKOUT} exact>
+                <Checkout />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.CHECKOUTCONFIRMED}>
+                <CheckoutConfirm />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.PAYMENTS} exact>
+                <Payments />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.PAYMENTSADDCARD}>
+                <PaymentsAddCard />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.MYACCOUNT}>
+                <MyAccount />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.SEMSESSION} exact>
+                <SemSession />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.SESSIONSTATE} exact>
+                <SessionState />
+              </PrivateRoute>
+              <PrivateRoute path={ROUTES.SEMHANDBOOK}>
+                <SemHandbook />
+              </PrivateRoute>
+              <Route path={ROUTES.FAQ}>
+                <FAQ />
+              </Route>
+              <Route path={ROUTES.HOME} exact>
+                <Home />
+              </Route>
+            </Switch>
+          </main>
+          <Footer />
+        </ConnectedRouter>
+      </Suspense>
     </AppWrapper>
   );
 };
