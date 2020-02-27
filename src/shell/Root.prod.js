02f6f00d2f640401f2dc446be8dedf7fdb650a51
Carlos Puga <carlos.puga@rootstrap.com>
2020-02-27 16:31:02 -0500

    Revert "feat(*): Adds forced https redirect"
    This reverts commit eede4d2605823d3b1a454e97b4be43a6fb7d12e3.

--- src/shell/Root.prod.js
+++ src/shell/Root.prod.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import HttpsRedirect from 'react-https-redirect';
-
 import configureStore from 'shared/store/configureStore';
 import { PersistGate } from 'redux-persist/integration/react';
 import Routes from './app/Routes';
@@ -10,13 +8,11 @@ const { store, persistor } = configureStore();
 
 const Root = () => {
   return (
-    <HttpsRedirect>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Routes />
-        </PersistGate>
-      </Provider>
-    </HttpsRedirect>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
   );
 };
 
