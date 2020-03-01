b90e74fa4e80d846bdd100185edebca4c02a4f4b
Carlos Puga <carlos.puga@rootstrap.com>
2020-02-29 20:33:53 -0500

    fix(*): Adds service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactGA from 'react-ga';
 import App from 'shell/Root';
+import * as serviceWorker from './serviceWorker';
 
 import 'shared/styles/fonts.css';
 import 'shared/styles/bootstrap.scss';
@@ -12,3 +13,4 @@ const GOOGLE_ANALYTICS_CODE = process.env.REACT_APP_GOOGLE_ANALYTICS_CODE;
 
 ReactGA.initialize(GOOGLE_ANALYTICS_CODE);
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
