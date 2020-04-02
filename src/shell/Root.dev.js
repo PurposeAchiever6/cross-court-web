94d40d724e9a34482f89ac922a436c604dd32628
Carlos Puga <carlos.puga@rootstrap.com>
2020-04-02 11:04:28 -0300

    fix(*): Disables eslint rules for certain files

--- src/shell/Root.dev.js
+++ src/shell/Root.dev.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-named-as-default-member */
+
 import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from 'shared/store/configureStore';
