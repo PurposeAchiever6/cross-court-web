101a065782d17bd13caf213bc63ec8bb563d3d42
Areg Abcarians <areg.abc@gmail.com>
2020-07-23 22:46:42 -0700

    Added Instagram feed plugin.

--- src/screens/homepage/components/desktop/FollowUsOnInstagram.jsx
+++ src/screens/homepage/components/desktop/FollowUsOnInstagram.jsx
@@ -7,7 +7,9 @@ const FollowUsOnInstagram = () => (
       <p className="title-2 shapiro95_super_wide">INSTAGRAM</p>
     </section>
     <div className="instafeed">
-      <img
+      <script src="https://apps.elfsight.com/p/platform.js"></script>
+      <div class="elfsight-app-a2f7cee6-3c03-4924-80fd-3fa237a1cc65"></div>
+      {/* <img
         alt="Shared by Crosscourt on June 01, 2020 tagging @blklivesmatter."
         src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/e35/102418911_888059024993304_111255001136287740_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&amp;_nc_cat=100&amp;_nc_ohc=hJqhiIIDOKEAX_cHP9D&amp;oh=fdb8a37b6168ebeced437edd4ca7db4e&amp;oe=5F300C21"
       />
@@ -18,16 +20,16 @@ const FollowUsOnInstagram = () => (
       <img
         alt="Shared by Crosscourt in Los Angeles, California with @paxfuller, @matttyyy_d, and @iamstansmith."
         src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/84354337_472298490323861_5482521474229322731_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&amp;_nc_cat=105&amp;_nc_ohc=gFVr9WMGJNYAX-mj23-&amp;oh=1a0430c160f8212c3a648958f4f096bd&amp;oe=5F2F4EF4"
-      />
+      /> */}
     </div>
-    <a
+    {/* <a
       className="ar-button load-more"
       href="https://www.instagram.com/crosscourt/"
       target="_blank"
       rel="noopener noreferrer"
     >
       <div className="ar-button-inner">LOAD MORE</div>
-    </a>
+    </a> */}
   </section>
 );
 
