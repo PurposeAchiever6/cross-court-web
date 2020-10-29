ea6e6798d0a2ca6e2218aeeef226a46cd90e14aa
Areg Abcarians <areg.abc@gmail.com>
2020-10-28 21:52:38 -0700

    Adding dispatch to useEffect to ensure page is loaded properly.

--- src/screens/survey/SurveyPage.jsx
+++ src/screens/survey/SurveyPage.jsx
@@ -1,22 +1,23 @@
 import React, { useEffect } from 'react';
 import { getIsAuthenticated } from 'screens/auth/reducer';
 import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ROUTES from 'shared/constants/routes';
 
 const SurveyPage = () => {
-    const history = useHistory();
-    const isAuthenticated = useSelector(getIsAuthenticated);
-  
-    useEffect(() => {
-      history.push(isAuthenticated ? ROUTES.HOME : ROUTES.LOGIN);
-    }, [isAuthenticated]);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const isAuthenticated = useSelector(getIsAuthenticated);
 
-    return (
-      <>
-      </>
-    );
-  }
-  
-  export default SurveyPage;
+  useEffect(() => {
+    history.push(isAuthenticated ? ROUTES.HOME : ROUTES.LOGIN);
+  }, [dispatch, isAuthenticated]);
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default SurveyPage;
   
