ed9cd90fa7193c7805c613c32bd8ed49850bb52f
Areg Abcarians <areg.abc@gmail.com>
2020-11-10 16:45:32 -0800

    Improving PWA experience.

--- src/screens/pwa/PWAPage.jsx
+++ src/screens/pwa/PWAPage.jsx
@@ -12,7 +12,13 @@ import { isIOS, isMobileSafari, isChrome, isAndroid } from 'react-device-detect'
 const PWAPage = () => {
   const isInstalled = window.matchMedia('(display-mode: standalone)').matches ||
     window.navigator.standalone === true;
-
+  const androidChrome = isAndroid && isChrome;
+  const iosSafari = isIOS && isMobileSafari;
+  const iosChrome = isIOS && isChrome;
+  const pwaSupported = androidChrome || iosSafari;
+  const pwaAndroidChromeMessage = <span>GO TO SETTINGS AND ADD TO HOME SCREEN</span>;
+  const pwaIOSSafariMessage = <span>TAP <span className="home-icon"></span> AND CLICK "ADD TO HOME SCREEN"</span>;
+  const pwaMessage = pwaSupported ? (androidChrome ? pwaAndroidChromeMessage : pwaIOSSafariMessage) : false;
   const dispatch = useDispatch();
   const history = useHistory();
   const isAuthenticated = useSelector(getIsAuthenticated);
@@ -23,7 +29,7 @@ const PWAPage = () => {
   useEffect(() => {
     dispatch(initialLoadInit());
 
-    if (isInstalled || (!isIOS && !isAndroid)) {
+    if (isInstalled || !pwaSupported) {
       history.push(ROUTES.HOME);
     }
   }, [dispatch]);
@@ -35,7 +41,7 @@ const PWAPage = () => {
         <p className="install">INSTALL</p>
         <p className="application">APPLICATION</p>
       </div>
-      <p className="message">{ isIOS && <span>TAP <span className="home-icon"></span> AND</span> } CLICK "ADD TO HOME SCREEN"</p>
+      <p className="message">{ pwaMessage }</p>
     </div>
   );
 }
