821f5a62a3db10540c51b4767ebbabad7312a87a
Colt McLaughlin <shuqinnq08@gmail.com>
2021-06-08 12:26:45 -0300

    Fix perks on medium screen

--- src/screens/sem/components/Landing/CCTeamPerks.jsx
+++ src/screens/sem/components/Landing/CCTeamPerks.jsx
@@ -15,7 +15,7 @@ const images = [
 ];
 
 const CCTeamPerks = () => (
-  <div className="flex flex-wrap md:px-80 my-20 justify-between">
+  <div className="flex flex-wrap md:px-40 2xl:px-80 my-20 justify-between">
     {images.map((image) => (
       <div className="flex flex-col justify-between items-center w-1/2 last:w-full h-44 mb-10 md:last:w-auto md:w-auto md:mb-0">
         <img src={image.src} alt="" />
