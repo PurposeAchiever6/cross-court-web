5d9c0dc848544927147d2e9fe57f3511af333774
Colt McLaughlin <shuqinnq08@gmail.com>
2022-01-21 20:54:07 -0300

    Fix warning

--- src/external-tools/gtm.js
+++ src/external-tools/gtm.js
@@ -8,7 +8,7 @@ if (env.REACT_APP_GTM_ENABLED === 'true') {
     w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
     var f = d.getElementsByTagName(s)[0],
       j = d.createElement(s),
-      dl = l != 'dataLayer' ? '&l=' + l : '';
+      dl = l !== 'dataLayer' ? '&l=' + l : '';
     j.async = true;
     j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
     f.parentNode.insertBefore(j, f);
