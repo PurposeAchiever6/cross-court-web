e3932d0b945a2120e5b966e535040b24be6f849a
Colt McLaughlin <shuqinnq08@gmail.com>
2022-03-05 14:23:32 -0300

    [498] [507] - Home screen carousel

--- src/screens/homepage/components/sections/MakeSportYourLifestyle.jsx
+++ src/screens/homepage/components/sections/MakeSportYourLifestyle.jsx
@@ -1,80 +1,63 @@
 import React from 'react';
-import PrimaryButton from 'shared/components/buttons/PrimaryButton';
 import ROUTES from 'shared/constants/routes';
-import styled from 'styled-components';
-import sportsLifeStyle from 'screens/homepage/images/basketball-gym-lockers.jpeg';
-import basketballGymGuy from 'screens/homepage/images/basketball-gym-guy.png';
-import basketballGym from 'screens/homepage/images/basketball-gym.jpeg';
 
-const Section = styled.section`
-  background-image: url('${basketballGym}');
-  background-position: center;
+import PrimaryButton from 'shared/components/buttons/PrimaryButton';
+import Carousel from 'shared/components/Carousel';
 
-  .animate {
-    animation: 2000ms ease-out 1000ms 1 both topFadeInAndSlideIn;
-  }
-
-  @media (min-width: 1024px) {
-    background-image: url('${sportsLifeStyle}');
-    background-position: 0 -350px;
-
-    .title-left {
-      width: 50%;
-      text-align: right;
-      padding-right: 7%;
-    }
-
-    .title-right {
-      width: 50%;
-      text-align: left;
-      padding-left: 6%;
-    }
-
-    .buttons {
-      position: absolute;
-      left: 50%;
-      z-index: 10;
-      padding-left: 3%;
-      padding-top: 4.5rem;
-    }
-  }
-
-  @media (min-width: 1280px) {
-    .buttons {
-      padding-left: 12%;
-    }
-  }
-`;
+import frontDesk from 'screens/homepage/images/front-desk.jpeg';
+import losAngelesBasketballCourt from 'screens/homepage/images/los-angeles-basketball-court.jpeg';
+import pickupBasketballFacility from 'screens/homepage/images/pickup-basketball-facility.jpeg';
+import pickupBasketballFacility2 from 'screens/homepage/images/pickup-basketball-facility-2.jpeg';
+import pickupBasketball from 'screens/homepage/images/pickup-basketball.jpeg';
+import lockers from 'screens/homepage/images/lockers.jpeg';
 
 const MakeSportYourLifestyle = () => (
-  <Section className="relative bg-cc-black bg-no-repeat bg-cover md:bg-fixed min-h-screen overflow-hidden flex flex-col justify-center px-4">
-    <h1 className="font-shapiro95_super_wide text-white text-center text-3xl lg:text-4xl xl:text-5xl mb-12 lg:mb-0 lg:flex lg:whitespace-nowrap lg:-mt-20 animate">
-      <span className="title-left block">MAKE SPORT</span>
-      <span className="title-right block">
-        YOUR <em className="shapiro96_inclined_wide">LIFESTYLE</em>
-      </span>
-    </h1>
-    <div className="buttons animate">
-      <div className="flex flex-col sm:flex-row justify-center items-center">
-        <PrimaryButton
-          contentClasses="w-52 sm:mr-8 mb-2 sm:mb-0"
-          to={ROUTES.HOWITWORKS}
-          bg="transparent"
-          color="white"
-        >
-          FIRST TIME?
-        </PrimaryButton>
-        <PrimaryButton contentClasses="w-52" to={ROUTES.LOCATIONS}>
-          BOOK SESSION
-        </PrimaryButton>
+  <div className="relative">
+    <Carousel
+      autoPlay
+      interval={5000}
+      className="w-screen h-screen"
+      imagesClassName="w-screen h-screen object-cover"
+      imageUrls={[
+        pickupBasketballFacility2,
+        pickupBasketballFacility,
+        losAngelesBasketballCourt,
+        frontDesk,
+        pickupBasketball,
+        lockers,
+      ]}
+    />
+    <div className="flex flex-col absolute w-full h-full bg-cc-black bg-opacity-40 top-0">
+      <div className="absolute top-1/3 px-10 md:px-20">
+        <h1 className="text-transparent text-stroke-white text-stroke-width-2 text-lg md:text-6xl font-shapiro96_inclined_wide">
+          <span className="hidden md:block">MAKE SPORT YOUR LIFESTYLE</span>
+          <span className="block md:hidden text-2xl">
+            MAKE SPORT <br /> YOUR LIFESTYLE
+          </span>
+        </h1>
+        <h2 className="text-white font-shapiro95_super_wide text-lg md:text-6xl">
+          BASKETBALL BASED FITNESS
+        </h2>
+        <h3 className="text-white text-sm md:text-2xl">
+          Pickup inspired experience. 1 hour. 15 players. 2 oficials. Games to 11. Tiered skill
+          levels.
+        </h3>
+        <div className="flex flex-col md:flex-row mt-10">
+          <PrimaryButton
+            contentClasses="w-52 sm:mr-8 mb-2 sm:mb-0"
+            to={ROUTES.HOWITWORKS}
+            bg="transparent"
+            color="white"
+          >
+            FIRST TIME?
+          </PrimaryButton>
+          <PrimaryButton contentClasses="w-52" to={ROUTES.LOCATIONS}>
+            BOOK SESSION
+          </PrimaryButton>
+        </div>
       </div>
     </div>
-    <img
-      alt="make-sports-your-lifestyle"
-      src={basketballGymGuy}
-      className="absolute hidden lg:block top-4 left-1/2 transform -translate-x-1/2 pl-6"
-    />
-  </Section>
+  </div>
 );
 
 export default MakeSportYourLifestyle;
--- src/shared/components/Carousel.jsx
+++ src/shared/components/Carousel.jsx
@@ -3,16 +3,28 @@ import { Carousel as ReactResponsiveCarousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import PropTypes from 'prop-types';
 
-const Carousel = ({ imageUrls, className, imagesClassName }) => (
+const Carousel = ({
+  imageUrls,
+  className,
+  imagesClassName,
+  infiniteLoop,
+  showArrows,
+  showStatus,
+  showThumbs,
+  autoPlay,
+  interval,
+}) => (
   <ReactResponsiveCarousel
     className={className}
-    infiniteLoop={true}
-    showArrows={true}
-    showStatus={false}
-    showThumbs={false}
+    infiniteLoop={infiniteLoop}
+    showArrows={showArrows}
+    showStatus={showStatus}
+    showThumbs={showThumbs}
+    autoPlay={autoPlay}
+    interval={interval}
   >
     {imageUrls.map((image, index) => (
-      <img className={imagesClassName} src={image} alt="" key={index} />
+      <img className={imagesClassName} src={image} alt={`carousel-${index}`} key={index} />
     ))}
   </ReactResponsiveCarousel>
 );
@@ -21,12 +33,24 @@ Carousel.defaultProps = {
   imageUrls: [],
   className: '',
   imagesClassName: '',
+  infiniteLoop: true,
+  showArrows: true,
+  showStatus: false,
+  showThumbs: false,
+  autoPlay: false,
+  interval: 3000,
 };
 
 Carousel.propTypes = {
   imageUrls: PropTypes.array,
   className: PropTypes.string,
   imagesClassName: PropTypes.string,
+  infiniteLoop: PropTypes.bool,
+  showArrows: PropTypes.bool,
+  showStatus: PropTypes.bool,
+  showThumbs: PropTypes.bool,
+  autoPlay: PropTypes.bool,
+  interval: PropTypes.number,
 };
 
 export default Carousel;
