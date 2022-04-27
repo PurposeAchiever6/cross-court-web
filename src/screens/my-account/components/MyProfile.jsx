8a64987894a8bd3a3c0db224ed9bd49b3000df6a
Felipe Fava <felipe.fava@moove-it.com>
2022-04-27 20:42:14 -0300

    [499] - Add referral code copy button

--- src/screens/my-account/components/MyProfile.jsx
+++ src/screens/my-account/components/MyProfile.jsx
@@ -1,30 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import { object, bool } from 'prop-types';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import EditIcon from 'shared/components/svg/EditIcon.svg';
+import { useHistory, useLocation } from 'react-router-dom';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
+
+import ROUTES from 'shared/constants/routes';
 import { formatPhoneNumber } from 'shared/utils/helpers';
+import missingProfileImg from 'shared/images/missing-profile-image.png';
+import EditIcon from 'shared/components/svg/EditIcon.svg';
+import PrimaryButton from 'shared/components/buttons/PrimaryButton';
+
 import { editProfileInit, showEditProfile } from '../actionCreators';
 import { getEditProfileLoading, getShowEditProfile } from '../reducer';
-
 import EditProfileForm from './EditProfileForm';
-import PrimaryButton from 'shared/components/buttons/PrimaryButton';
-import { useHistory, useLocation } from 'react-router-dom';
-import ROUTES from 'shared/constants/routes';
-import missingProfileImg from 'shared/images/missing-profile-image.png';
-
-const MyProfileContainer = styled.div`
-  > button {
-    padding: 0;
-    margin: 0;
-    background-color: transparent;
-    border: 0;
-    position: absolute;
-    top: 2.4rem;
-    right: 2rem;
-    cursor: pointer;
-  }
-`;
 
 const detailRowClasses = 'flex flex-col mb-8 text-lg';
 const titleClasses = 'font-shapiro95_super_wide';
@@ -34,6 +24,8 @@ const MyProfile = ({ profile, showTitle = true }) => {
   const history = useHistory();
   const location = useLocation();
 
+  const [inviteFriendTextCopied, setInviteFriendTextCopied] = useState(false);
+
   const editProfileAction = (values) => dispatch(editProfileInit(values));
   const showEditProfileAction = () => dispatch(showEditProfile());
 
@@ -42,10 +34,13 @@ const MyProfile = ({ profile, showTitle = true }) => {
 
   const dateOfBirth = new Date(profile.birthday);
   const defaultPaymentMethod = profile.defaultPaymentMethod;
+  const inviteFriendText =
+    `Use my referral code ${profile.referralCode} at checkout to receive 50% off ` +
+    'on your first month of any Crosscourt membership';
 
   return (
-    <MyProfileContainer className="relative p-8">
-      <button type="button" onClick={showEditProfileAction}>
+    <div className="relative p-8">
+      <button type="button" onClick={showEditProfileAction} className="absolute right-8 top-10">
         <EditIcon />
       </button>
       {showTitle && <h3 className="font-shapiro95_super_wide mb-3 text-3xl">MY ACCOUNT</h3>}
@@ -134,18 +129,24 @@ const MyProfile = ({ profile, showTitle = true }) => {
               profile.ccCash
             )}`}</span>
           </div>
+          <CopyToClipboard onCopy={() => setInviteFriendTextCopied(true)} text={inviteFriendText}>
+            <PrimaryButton w="100%" className="mb-2">
+              <FontAwesomeIcon icon={faExternalLinkAlt} />{' '}
+              {inviteFriendTextCopied ? 'Copied' : 'Invite a friend'}
+            </PrimaryButton>
+          </CopyToClipboard>
           <PrimaryButton to={ROUTES.REFERRALS} inverted w="100%">
             Referral History
           </PrimaryButton>
         </>
       )}
-    </MyProfileContainer>
+    </div>
   );
 };
 
 MyProfile.propTypes = {
-  profile: object.isRequired,
-  showTitle: bool,
+  profile: PropTypes.shape().isRequired,
+  showTitle: PropTypes.bool,
 };
 
 export default MyProfile;
