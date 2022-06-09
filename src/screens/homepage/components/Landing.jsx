e1cf2131651d2ed17904aec780bc5c6c055517eb
Colt McLaughlin <shuqinnq08@gmail.com>
2022-06-09 19:28:01 -0300

    [638] - Replace the memberships perk section

--- src/screens/homepage/components/Landing.jsx
+++ src/screens/homepage/components/Landing.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import CrosscourtKeys from 'shared/components/CrosscourtKeys';
+import MembershipsFeatures from 'shared/components/MembershipsFeatures';
 import UnleashYourInnerAthlete from './sections/UnleashYourInnerAthlete';
 import EveryoneIsAnThlete from './sections/EveryoneIsAnThlete';
 import WhatWeDo from './sections/WhatWeDo';
-import MembershipsFeatures from './sections/MembershipsFeatures';
 import Testimonials from './sections/Testimonials';
 import FollowUsOnInstagram from './sections/FollowUsOnInstagram';
 import Ready from './sections/Ready';
