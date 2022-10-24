1455dc9636661d32b15e428ab8fa2065852cd480
Colt McLaughlin <shuqinnq08@gmail.com>
2022-10-24 19:39:51 -0300

    Change wording of session guest added modal

--- src/screens/sessions/components/modals/AddGuestModal/GuestAddedStep.jsx
+++ src/screens/sessions/components/modals/AddGuestModal/GuestAddedStep.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const GuestAddedStep = () => (
   <div className="text-center">
-    Your guest will receive a text message confirming their invite. They will be asked to show their
-    I.D. at check in.
+    Your guest will receive a text message confirming their invite to join you. The message will
+    include an access code that will need to be presented to one of our Experience Manager's upon
+    arrival. Guests can be added/removed through the session details page found in "My Account".
   </div>
 );
 
