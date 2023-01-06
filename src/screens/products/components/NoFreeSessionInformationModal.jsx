ef600ddd4d0c39d2e23c763c848cc859549a3acd
Colt McLaughlin <shuqinnq08@gmail.com>
2023-01-06 12:34:33 -0300

    Update first free session price

--- src/screens/products/components/NoFreeSessionInformationModal.jsx
+++ src/screens/products/components/NoFreeSessionInformationModal.jsx
@@ -10,7 +10,7 @@ export const NoFreeSessionInformationModal = ({ isOpen, closeHandler }) => (
       <div className="mb-8">
         <p className="mb-4">
           Due to your location, you are not eligible for a free trial session. If you wish to
-          proceed, you can buy a credit session at a <strong>discounted price of $12</strong>.
+          proceed, you can buy a credit session at a <strong>discounted price of $16</strong>.
         </p>
         <p>
           You can purchase this session credit later by going to the <strong>"Memberships"</strong>{' '}
