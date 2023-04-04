5b3090a56c4144e81455e0205f18766bc8d579f8
Colt McLaughlin <shuqinnq08@gmail.com>
2023-04-04 16:32:56 -0300

    Fix issue: https://github.com/vitejs/vite/issues/2139. Solution: https://github.com/vitejs/vite/issues/2139\#issuecomment-1405624744

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,32 @@
+/* eslint-disable import/no-dynamic-require */
 import { defineConfig } from 'vite';
 import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig(() => ({
   server: { port: 3000 },
+  plugins: [viteCommonjs(), react()],
   build: {
     outDir: 'build',
+    commonjsOptions: {
+      defaultIsModuleExports(id) {
+        try {
+          const module = require(id);
+          if (module?.default) {
+            return false;
+          }
+          return 'auto';
+        } catch (error) {
+          return 'auto';
+        }
+      },
+      transformMixedEsModules: true,
+    },
   },
-  plugins: [react()],
   resolve: {
     alias: [
       { find: 'shell', replacement: fileURLToPath(new URL('./src/shell', import.meta.url)) },
