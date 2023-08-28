4ceba8d2c9e7201e63c9e25e34ab0844d0d300d9
Colt McLaughlin <shuqinnq08@gmail.com>
2023-08-28 13:31:52 -0300

    Only enable google ads if gtm enabled

--- src/shell/app/HtmlHead.jsx
+++ src/shell/app/HtmlHead.jsx
@@ -7,15 +7,22 @@ const GOOGLE_ADS_CODE = import.meta.env.VITE_GOOGLE_ADS_CODE;
 const HtmlHead = () => (
   <Helmet>
     <script type="application/ld+json">{faqJson}</script>
-    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_CODE}`}></script>
-    <script>
-      {`
+    {import.meta.env.VITE_GTM_ENABLED === 'true' && (
+      <>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_CODE}`}
+        ></script>
+        <script>
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', "${GOOGLE_ADS_CODE}");
         `}
-    </script>
+        </script>
+      </>
+    )}
   </Helmet>
 );
 
