32a5541430636acccb42bd6858558bd07c8eb2e8
Colt McLaughlin <shuqinnq08@gmail.com>
2023-09-22 01:05:08 -0500

    [Ampli #3] feat: add user group constants (1-10)

---
+++ src/shared/constants/userGroup.js
@@ -0,0 +1,12 @@
+export default {
+  EMPLOYMENT_STATUS: { type: 'Employment Status', name: ['Employee Group', 'Non-Employee Group'] },
+  MEMBERSHIP_DURATION: { type: 'Membership Duration', name: ['New Members', 'Original Members'] },
+  AGE_RANGE: { type: 'Age Range', name: ['Teens', 'Young Adults', 'Middle-Aged', 'Seniors'] },
+  LOCATION: { type: 'Location' },
+  USER_INTERESTS: { type: 'User Interests', name: ['Sports Fans', 'Music Lovers', 'Movie Buffs', 'Gamers'] },
+  USER_ACTIVITY_LEVEL: { type: 'User Activity Level', name: ['Active Users', 'Occasional Users', 'Inactive Users'] },
+  USERROLE: { type: 'UserRole', name: ['Admins', 'Moderators', 'Regular Users', 'Guests'] },
+  SUBSCRIPTION_TYPE: { type: 'Subscription Type', name: ['Free Users', 'Premium Users', 'Trial Users'] },
+  CUSTOMER_TYPE: { type: 'Customer Type', name: ['First-Time Customers', 'Repeat Customers', 'Loyal Customers'] },
+  EXPERTISE_LEVEL: { type: 'Expertise Level', name: ['Beginners', 'Intermediate Users', 'Advanced Users', 'Experts'] },
+};
